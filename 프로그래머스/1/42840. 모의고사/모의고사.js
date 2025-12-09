0fb2b169762ec06583698eaa5180d235fe5a31ed
로인 <151013952+bboroin@users.noreply.github.com>
2025-12-09 17:55:12 +0900

    [level 1] Title: 모의고사, Time: 32.37 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,25 @@
+function solution(answers) {
+    var answer = [];
+    let scores = [0, 0, 0];
+    const patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    
+    for (const [i, answer] of answers.entries()) {
+        for (const [j, pattern] of patterns.entries()) {
+            if (answer === pattern[i % pattern.length]) {
+                scores[j] += 1;
+            }
+        }
+    }
+    
+    const highestScores = [];
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] === Math.max(...scores)) {
+            highestScores.push(i + 1);
+        }
+    }
+    return highestScores;
+}
