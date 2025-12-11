995600f55c13e458684006fa3a7a817f2b5b3b24
로인 <151013952+bboroin@users.noreply.github.com>
2025-12-11 13:43:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 110.68 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,21 +1,15 @@
 function solution(number, limit, power) {
     var answer = 0;
-    const knights = []
-    let divisorCount = 0;
+    
     for (let i=1; i<=number; i++) {
+        let divisorCount = 0;
         for (let j=1; j<=Math.sqrt(i); j++) {
             if (i % j === 0) {
                 divisorCount++;
-                if (j !== i / j) divisorCount++;;
+                if (j !== i / j) divisorCount++;
             } 
         }
-        knights.push(divisorCount);
-        divisorCount = 0; 
-    }
-    for (let i=0; i<knights.length; i++) {
-        if (knights[i] > limit) answer += power;
-        else answer +=knights[i];
-    }
-    
+        divisorCount > limit ? answer += power : answer += divisorCount;
+    }    
     return answer;
 }
